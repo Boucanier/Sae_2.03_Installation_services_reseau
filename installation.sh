f6070e825dab704cedc0834df8e4a69e3d143963
Jules C <118428520+Boucanier@users.noreply.github.com>
2023-06-06 16:30:55 +0200

    Démarrage des bons service et ouverture du port http

--- installation.sh
+++ installation.sh
@@ -5,3 +5,8 @@ yum install mysql-server
 yum install php
 yum install php-mysqli
 yum install php-gd
+
+systemctl start mysqld.service
+systemctl start firewalld.service
+
+firewall-cmd --zone=public --add-service=http
