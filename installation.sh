87d5645ecb18d9332ec43e800a8da017ae4b6127
Jules C <118428520+Boucanier@users.noreply.github.com>
2023-06-11 20:33:46 +0200

    Corrections

--- installation.sh
+++ installation.sh
@@ -11,7 +11,7 @@ systemctl start firewalld.service
 
 firewall-cmd --zone=public --add-service=http
 
-mv ../Sae_2.03_Installation_services-reseau_main ../S203
+mv ../Sae_2.03_Installation_services_reseau-main ../S203
 mv ../S203 /var/www/html/
 
 mysql -e "source /var/www/html/S203/creation_s203.sql"
